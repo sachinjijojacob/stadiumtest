751197f8bf9dd1d53d96512a7408ed9bc088b66b
Sachin Jacob <s.jacob@easternenterprise.com>
2018-06-21 17:08:36 +0530

    created a landing page and route fixed

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, TemplateRef} from '@angular/core';
+import {BsModalRef} from 'ngx-bootstrap/modal/bs-modal-ref.service';
+import {MainService, Stadium} from '../main-service.service';
+import {BsModalService} from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-landing-page',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing-page.component.css']
 })
 export class LandingPageComponent implements OnInit {
-
-  constructor() { }
-
+  modalRef: BsModalRef;
+  stadiumData: Stadium[];
+  selectedStadium:  Stadium[];
+  constructor(private modalService: BsModalService, private  mainservice: MainService) {}
   ngOnInit() {
+    this.stadiumData = this.mainservice.getData();
+    console.log(this.stadiumData);
+
   }
 
+  openModal(template: TemplateRef<any>, s: any) {
+    this.selectedStadium  = s;
+    this.modalRef = this.modalService.show(template);
+  }
 }
